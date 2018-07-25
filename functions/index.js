0238a372e5f25eaddab1d46c943087a83966c981
Edgar Montes <jedgar1mx@gmail.com>
2018-07-25 14:27:16 -0400

    setting up firebase function

--- functions/index.js
+++ functions/index.js
@@ -1,35 +1,36 @@
 const functions = require('firebase-functions');
-
+const requestGlobal = require('request');
+const cors = require('cors')({
+    origin: true,
+});
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+// //
 
 exports.getToken = functions.https.onRequest((request, response) => {
-    let url = 'https://gis.detroitmi.gov/arcgis/tokens/generateToken';
-    let param = {
-        username: 'montesje_detroitmi',
-        password: 'Mango2017',
-        referer: 'https://localhost:3000/',
-        f: 'json'
-    }
-    let postRequest = new Request(url, {
-        method: 'POST',
-        body: JSON.stringify(param),
-        headers: new Headers({
-          'Content-type': 'application/json'
-        }),
-        mode: 'cors',
-        cache: 'default'
+
+    return cors(request, response, ()=>{
+        var options = { method: 'POST',
+        url: 'https://www.arcgis.com/sharing/rest/oauth2/token',
+        headers:
+        { 
+            'content-type': 'application/x-www-form-urlencoded',
+            'Access-Control-Allow-Origin': '*',
+            "Access-Control-Allow-Methods": "POST, GET, PUT, DELETE, OPTIONS",
+            "Access-Control-Allow-Credentials":  false,
+            "Access-Control-Allow-Headers": "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+        },
+        form:
+        { client_id: functions.config().localserviceslookup.id,
+        client_secret: functions.config().localserviceslookup.secret,
+        grant_type: 'client_credentials' } };
+        
+        requestGlobal(options, function (error, results, body) {
+            if (error) throw new Error(error);
+
+            console.log(body);
+            response.send(body);
+        });
     });
-    fetch(postRequest)
-    .then((resp) => {
-        console.log(resp);
-        // console.log(resp.status);
-        if(resp.status === 201){
-            // console.log('item submitted');
-        }
-    });
+    
 });
