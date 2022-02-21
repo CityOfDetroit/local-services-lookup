c6469dbfbfbed20f30d5418c7f85efa90aff01d5
Edgar Montes <jedgar1mx@gmail.com>
2022-02-21 17:36:05 -0500

    Adding travis build test.

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = function(_env, argv) {
     output: {
       path: path.resolve(__dirname, "build"),
       filename: "assets/js/[name].js",
-      publicPath: "/"
+      publicPath: ""
     },
     module: {
       rules: [
