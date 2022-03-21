013eccea62a2ff6c2533e950cf58ab10dc0f1d80
Edgar Montes <jedgar1mx@gmail.com>
2022-03-21 13:10:50 -0400

    Handling wrong address better.

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -13,4 +13,15 @@ describe('Basic test for address result', () => {
       cy.wait(1000)
       cy.get('#geocoder input').type('{enter}')
     })
+
+    it('Test invalid address', () => {
+      cy.visit('/') // change URL to match your dev URL
+      cy.contains('My Home Info')
+      cy.get('#addresses-list').should('be.empty')
+      cy.get('#geocoder input').type('1222 test')
+      cy.wait(1000)
+      cy.get('#geocoder input').type('{enter}')
+      cy.wait(3000)
+      cy.contains('No Information found.')
+    })
 })
