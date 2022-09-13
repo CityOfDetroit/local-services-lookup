1c0fd6bcb408f48dc04d3261356ed3ece392ce59
Edgar Montes <jedgar1mx@gmail.com>
2022-09-13 15:16:12 -0400

    Loaded initial screen and active geocoder screen.

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,6 +1,10 @@
 'use strict';
 
 export default class Display extends HTMLElement {
+    static get observedAttributes() {
+        return ['data-display-type'];
+    }
+
     constructor() {
         // Always call super first in constructor
         super();
@@ -10,10 +14,54 @@ export default class Display extends HTMLElement {
 
         // Create result section
         const displayWrapper = document.createElement('section');
-        displayWrapper.id = 'app-display';
-        const geocoder = document.createElement('app-geocoder');
-        const resultsWrapper = document.createElement('section');
-        resultsWrapper.id = 'data-results';
-        shadow.appendChild(geocoder);
+        displayWrapper.id = 'display-wrapper';
+        console.log('adding text');
+        const text = document.createElement('p');
+        text.innerText = 'welcome screen';
+        displayWrapper.appendChild(text)
+        const btn = document.createElement('button');
+        btn.innerText = 'Start';
+        btn.addEventListener('click', (ev)=>{
+            console.log(ev);
+            const app = document.getElementsByTagName('my-home-info');
+            console.log(app[0]);
+            app[0].setAttribute('data-app-state', 'active-screen');
+        })
+        displayWrapper.appendChild(btn)
+        shadow.appendChild(displayWrapper);
     }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        console.log(`attribute: ${name}, old: ${oldValue}, new: ${newValue}`);
+        this.loadDisplay(this);
+    }
+
+    clearDisplay(display) {
+        console.log('clearing display');
+        const shadow = display.shadowRoot;
+        while (shadow.firstChild) {
+            shadow.removeChild(shadow.firstChild);
+        }
+    }
+
+    loadDisplay(display) {
+        const shadow = display.shadowRoot;
+        const displayWrapper = document.createElement('section');
+        displayWrapper.id = 'display-wrapper';
+        switch (this.getAttribute('data-display-type')) {
+            case 'welcome-screen':
+                const text = document.createElement('p');
+                text.innerText = 'welcome screen';
+                displayWrapper.appendChild(text);
+                shadow.appendChild(displayWrapper);
+                break;
+
+            case 'active':
+                break;
+        
+            default:
+                break;
+        }
+    }
+
 }
--- src/components/MyHomeInfo.js
+++ src/components/MyHomeInfo.js
@@ -5,6 +5,10 @@ customElements.define('app-display', Display);
 customElements.define('app-geocoder', Geocoder);
 
 export default class MyHomeInfo extends HTMLElement {
+    static get observedAttributes() {
+        return ['data-app-state'];
+    }
+
     constructor() {
         // Always call super first in constructor
         super();
@@ -12,14 +16,51 @@ export default class MyHomeInfo extends HTMLElement {
         // Create a shadow root
         const shadow = this.attachShadow({mode: 'open'});
 
-        // Get current setup
-        const dataAvailable = document.getElementById('active-data').value.split(',');
-
         // Create result section
         const appWrapper = document.createElement('section');
-        const geocoder = document.createElement('app-geocoder');
-        shadow.appendChild(geocoder);
+        appWrapper.id = 'app-wrapper';
         const display = document.createElement('app-display');
-        shadow.appendChild(display);
+        display.setAttribute('data-display-type', 'welcome');
+        appWrapper.appendChild(display);
+        shadow.appendChild(appWrapper);
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        const shadow = this.shadowRoot;
+        console.log(`attribute: ${name}, old: ${oldValue}, new: ${newValue}`);
+        if(oldValue != null){
+            this.clearApp(this);
+            this.loadApp(this);
+        }
+    }
+
+    clearApp(app) {
+        console.log('clearing app');
+        const shadow = app.shadowRoot;
+        while (shadow.firstChild) {
+            shadow.removeChild(shadow.firstChild);
+        }
+    }
+
+    loadApp(app) {
+        const shadow = app.shadowRoot;
+        const appWrapper = document.createElement('section');
+        appWrapper.id = 'app-wrapper';
+        const display = document.createElement('app-display');
+        switch (app.getAttribute('data-app-state')) {
+            case 'welcome-screen':
+                display.setAttribute('data-display-type', 'welcome');
+                appWrapper.appendChild(display);
+                break;
+
+            case 'active-screen':
+                const geocoder = document.createElement('app-geocoder');
+                appWrapper.appendChild(geocoder);
+                break;
+        
+            default:
+                break;
+        }
+        shadow.appendChild(appWrapper);
     }
 }
