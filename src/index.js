ed418eae7d50ff56e7ee042b5bc4b262c93b0cf9
Edgar Montes <jedgar1mx@gmail.com>
2023-05-24 15:43:02 -0400

    Adding CDN for local development.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
-
-// // ============ only for local dev ==================
-// import 'cod-design-system/src/components/atoms/Loader/cod-loader';
-// import 'cod-design-system/src/components/atoms/Button/cod-button';
-// // ==================================================
 import MyHomeInfo from './components/MyHomeInfo';
 customElements.define('my-home-info', MyHomeInfo);
